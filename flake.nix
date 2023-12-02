a2b5c50ad467945115d4ddd78055889fe4a9c721
Michael Augaitis <michael.aug@proton.me>
2023-12-02 10:21:17 +0000

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Rust Environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        with pkgs;
+        {
+
+          devShells.default = mkShell {
+
+            buildInputs = with nixpkgs; [
+              cargo
+              rustc
+              rustfmt
+              rust-analyzer
+              lldb
+            ];
+          };
+        }
+      );
+}
